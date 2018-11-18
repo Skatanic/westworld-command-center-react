3c4b77ae859d1ad47d7e6b69d278143ff08387eb
Moe <maurice.ta@gmail.com>
2018-11-18 09:35:25 -0500

    Adds lodash to help with string sanitizing

--- src/components/Area.js
+++ src/components/Area.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import startCase from 'lodash/startCase'
 
 const Area = ({ area }) => (
 
   <div className='area' id={area.name}>
-    <h3 className='labels'>{area.name}</h3>
+    <h3 className='labels'>{startCase(area.name)}</h3>
 
     {/* See Checkpoint 1 item 2 in the Readme for a clue as to what goes here */}
 
--- src/components/WestworldMap.js
+++ src/components/WestworldMap.js
@@ -5,7 +5,6 @@ import Area from './Area'
 
 const WestworldMap = ({ areas }) => {
   const areaContext = useContext(AreaContext)
-  console.log('areaConext', areaContext)
   return (
     <Segment id="map">
       {areaContext.map(area => <Area key={area.id} area={area} />)}
