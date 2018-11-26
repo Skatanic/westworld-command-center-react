03e4fb6f2e515a884a98821decab92214fe448b0
Moe <maurice.ta@gmail.com>
2018-11-26 09:12:57 -0500

    Edit selected host info

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,20 +1,20 @@
 import React, { memo, useContext } from 'react'
 import { Segment, Image } from 'semantic-ui-react'
 import * as Images from '../services/Images'
-// import HostInfo from './HostInfo'
+import HostInfo from './HostInfo'
 import { HostContext } from '../App'
 
 const Details = memo(() => {
-  const { selectedHost } = useContext(HostContext)
+  const { hosts } = useContext(HostContext)
+  const selectedHost = hosts.find(h => h.selected)
   // We'll render the logo if no host is selected. But if a host does get selected....
   // Watch the video to see how this works in the app.
   return(
     <Segment id="details" className="HQComps">
-    <Image size='medium' src={Images.westworldLogo} />
-      {/* {selectedHost 
-        ? <HostInfo host={selectedHost} />
+      {selectedHost 
+        ? <HostInfo selectedHost={selectedHost} />
         : <Image size='medium' src={Images.westworldLogo} />
-      } */}
+      }
     </Segment>
   )
 })
--- src/components/HostInfo.js
+++ src/components/HostInfo.js
@@ -1,49 +1,50 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
+import startCase from 'lodash/startCase'
 import { Radio, Icon, Card, Grid, Image, Dropdown, Divider } from 'semantic-ui-react'
-import { HostContext } from '../App'
+import { AreaContext, HostContext } from '../App'
 
-const HostInfo = () => {
+const HostInfo = ({ selectedHost }) => {
+  const areas = useContext(AreaContext)
+  const { editSelectedHost } = useContext(HostContext)
 
-  const handleChange = (e, {value}) => {
-    // the 'value' attribute is given via Semantic's Dropdown component.
-    // Don't worry too much about how this works.
-    // Just know that Semantic dropdowns take options as an array of objects in this form:
-    // {key: "some_text", text: "Some Text", value: "some_text"}
-    // You get access to the last one for whatever is selected
-    // See the Semantic docs for more info: https://react.semantic-ui.com/modules/dropdown/#usage-controlled
+  const handleChange = (e, { value }) => 
+    editSelectedHost({ ...selectedHost, area: value })
 
-    // Also, there's more info on this below
-  }
-
+  const toggleActive = (event, { checked }) =>
+    editSelectedHost({ ...selectedHost, active: checked })
 
   return (
     <Grid>
       <Grid.Column width={6}>
-        <Image style={{overflow: "hidden", height: "160px", width: "130px"}} floated='left' size='small' src={ /* pass in the right image here */ }/>
+        <Image style={{overflow: "hidden", height: "160px", width: "130px"}} floated='left' size='small' src={selectedHost.imageUrl}/>
       </Grid.Column>
       <Grid.Column width={10}>
         <Card>
           <Card.Content>
             <Card.Header>
-              {selectedHost.firstName} | { /* What needs to be true to render the man icon or false to render the woman icon? */ ? <Icon name='man' /> : <Icon name='woman' />}
+              {selectedHost.firstName} | {selectedHost.gender === 'Male' ? <Icon name='man' /> : <Icon name='woman' />}
             </Card.Header>
             <Card.Meta>
-              <Radio style={{margin: "10px"}} slider onChange={toggle} label={"AmIActive?"} checked={/* What happens when I'm checked */}/>
+              <Radio 
+                style={{margin: "10px"}} 
+                slider 
+                onChange={toggleActive} 
+                label={selectedHost.active ? 'Active' : 'Not Active'} 
+                checked={selectedHost.active} 
+              />
             </Card.Meta>
 
             <Divider />
             Current Area:
             <Dropdown
               onChange={handleChange}
-              value={/* This is the value of whatever is currently selected. See example below */}
-              {/*
-                Pass an array of objects to 'options' like so:
-                [{key: "area_one" text: "Area One" value: "area_two"}, {key: "area_two" text: "Area Two" value: "area_two"}]
-                The value should be set to whatever you want currently selected. Like "area_two".
-                The dropdown will display whatever corresponds to the test key, like "Area Two".
-                  */}
+              value={selectedHost.area}
               selection
-              options={/* These are your selection options. See above. */}
+              options={areas.map(area => ({
+                key: area.name,
+                text: startCase(area.name),
+                value: area.name
+              }))}
             />
           </Card.Content>
         </Card>
